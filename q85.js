1f8670aa2de610b5d11771a230bab0d9f34cfbc2
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-28 02:27:32 +0500

    File of q85

---
+++ q85.js
@@ -0,0 +1,7 @@
+//Finding the Position of a Substring: Write a function that locates the first occurrence of the word "code" within any given string and returns its position.
+function findCodePosition(str) {
+    return str.indexOf("code"); // Looks for "code" and tells where it found it
+}
+console.log(findCodePosition("Learn to code with JavaScript"));
+// Outputs the start position of "code"
+// It tells us the position number where "code" starts.
---
+++ q85.ts
@@ -0,0 +1,16 @@
+//Finding the Position of a Substring: Write a function that locates the first occurrence of the word "code" within any given string and returns its position.
+
+
+function findCodePosition(str:string) : number {
+    return str.indexOf("code");                     // Looks for "code" and tells where it found it
+}
+
+console.log(findCodePosition("Learn to code with JavaScript"));   
+
+// Outputs the start position of "code"
+
+
+
+
+
+// It tells us the position number where "code" starts.
