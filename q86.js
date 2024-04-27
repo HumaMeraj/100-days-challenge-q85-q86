39a94641338e85d605d7965751894dba2bdcb505
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-28 02:28:03 +0500

    File of q86

---
+++ q86.js
@@ -0,0 +1,6 @@
+//Checking for Text Presence: Create a function that checks if the word "JavaScript" is present in a given string. It should return true if found, otherwise false.
+function hasJavaScript(str) {
+    return str.includes("JavaScript");
+}
+console.log(hasJavaScript("I love coding in JavaScript!"));
+// It simply says true if "JavaScript" is there, or false if it's not.
